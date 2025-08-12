dce95b39b93902d7aa1d57b5ea7989634f738a98
Rayane300402 <raycaratboulsi@gmail.com>
2025-08-12 13:20:44 +0300

    created custom hook called useFetch to fetch data from a url

--- src/Components/UseFetch.jsx
+++ src/Components/UseFetch.jsx
@@ -1,5 +1,11 @@
+import { useEffect, useState } from "react";
+
 const UseFetch = (url) => {
-    
+    const [data, setData] = useState();
+    useEffect(() => {
+         fetch(url).then((res)=> res.json()).then((data)=> setData(data)).catch((error) => console.log(error)); 
+    }, [])
+    return [data];
 }
 
 export default UseFetch
